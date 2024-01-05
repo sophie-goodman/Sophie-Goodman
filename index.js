3f68d9106b20b9ac0d5f53d2b91a2e1f4444e802
sophie-goodman <sgsg100@live.com>
2024-01-05 16:04:44 -0500

    new stuff

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-$(".menuIcon").click(function(){
-    $(".menbar").toggle();
-});
 
+
+const name = document.querySelector('.name');
+animate(name);
+
+function animate(element){
+    let elementWidth = element.offsetWidth;
+    let parentWidth = element.parentElement.offsetWidth;
+    let flag = 0;
+
+    setInterval(() =>{
+        element.style.marginLeft = --flag + 'px';
+
+        if(elementWidth == -flag){
+            flag = parentWidth;
+        }
+} , 10);
+}
